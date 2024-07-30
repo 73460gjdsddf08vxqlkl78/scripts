3f683d004d54941f7a5c9b41f91d6da0d64aa0ec
Gh0u1L5 <Gh0u1L5@outlook.com>
2024-07-31 02:17:09 +0800

    Add support for multiple domains.

--- renew-certificate.py
+++ renew-certificate.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 import base64
 import io
+import json
 import os
 import zipfile
 from tencentcloud.common import credential
@@ -12,14 +13,6 @@ from tencentcloud.ssl.v20191205 import ssl_client, models
 
 HOME = "/opt/acme.sh"
 CONFIG_HOME = "/mnt/etc/acme.sh"
-
-###########################################################
-# SSL Certificate Config
-###########################################################
-
-DOMAIN = os.environ["DOMAIN"]
-CERTIFICATE_RESOURCE_TYPES = os.environ.get("CERTIFICATE_RESOURCE_TYPES", "apigateway").split(",")
-CERTIFICATE_RESOURCE_TYPES_REGIONS = os.environ.get("CERTIFICATE_RESOURCE_TYPES_REGIONS", "ap-hongkong").split(",")
 
 ###########################################################
 # Setup Tencent Cloud Client
@@ -36,15 +29,15 @@ TENCENT_SSL_CLIENT = ssl_client.SslClient(
 )
 
 
-def search_latest_certificate() -> str:
+def search_latest_certificate(domain: str) -> str:
     req = models.DescribeCertificatesRequest()
-    req.SearchKey = DOMAIN
+    req.SearchKey = domain
     req.ExpirationSort = "DESC"
     req.FilterSource = "upload"
     resp = TENCENT_SSL_CLIENT.DescribeCertificates(req)
     return resp.Certificates[0].CertificateId
 
-def download_certificate(certificateID: str) -> bytes:
+def download_certificate(domain: str,certificateID: str) -> bytes:
     req = models.DownloadCertificateRequest()
     req.CertificateId = certificateID
     resp = TENCENT_SSL_CLIENT.DownloadCertificate(req)
@@ -52,7 +45,7 @@ def download_certificate(certificateID: str) -> bytes:
     zipContent = base64.b64decode(resp.Content)
     with zipfile.ZipFile(io.BytesIO(zipContent)) as zipFile:
         zipFile.extractall("/tmp")
-        return zipFile.read(f"{DOMAIN}.pem")
+        return zipFile.read(f"{domain}.pem")
 
 def upload_certificate(keyPath: str, certificatePath: str):
     req = models.UploadCertificateRequest()
@@ -65,16 +58,16 @@ def upload_certificate(keyPath: str, certificatePath: str):
     resp = TENCENT_SSL_CLIENT.UploadCertificate(req)
     print(f"[*] Uploaded certificate. Response: {resp}")
 
-def update_certificate(certificateID: str, keyPath: str, certificatePath: str):
+def update_certificate(certificateID: str, types: list[str], types_regions: dict[str, list[str]], keyPath: str, certificatePath: str):
     req = models.UpdateCertificateInstanceRequest()
     req.OldCertificateId = certificateID
 
-    req.ResourceTypes = CERTIFICATE_RESOURCE_TYPES
+    req.ResourceTypes = types
     req.ResourceTypesRegions = list()
-    for t in CERTIFICATE_RESOURCE_TYPES:
+    for type, regions in types_regions.items():
         region = models.ResourceTypeRegions()
-        region.ResourceType = t
-        region.Regions = CERTIFICATE_RESOURCE_TYPES_REGIONS
+        region.ResourceType = type
+        region.Regions = regions
         req.ResourceTypesRegions.append(region)
 
     with open(keyPath, "r") as f:
@@ -89,20 +82,20 @@ def update_certificate(certificateID: str, keyPath: str, certificatePath: str):
     resp = TENCENT_SSL_CLIENT.UpdateCertificateInstance(req)
     print(f"[*] Updated certificate {certificateID}. Response: {resp}")
 
-def main_handler(event, context):
-    # Renew certificate.
-    os.system(f"{HOME}/acme.sh --cron --home {HOME} --config-home {CONFIG_HOME}")
+def handle_domain(domain: str, domain_config: dict):
+    types = domain_config.get("types", [])
+    types_regions = domain_config.get("types_regions", [])
 
     # Search latest certificate.
-    certificateID = search_latest_certificate()
+    certificateID = search_latest_certificate(domain)
     print(f"[*] Latest certificate ID: {certificateID}")
 
     # Download online certificate.
-    onlineCert = download_certificate(certificateID)
+    onlineCert = download_certificate(domain, certificateID)
     print(f"[*] Online certificate: {onlineCert}")
 
     # Check if the certificate is renewed.
-    with open(f"{CONFIG_HOME}/{DOMAIN}_ecc/fullchain.cer", "rb") as f:
+    with open(f"{CONFIG_HOME}/{domain}_ecc/fullchain.cer", "rb") as f:
         localCert = f.read()
         print(f"[*] Local certificate: {localCert}")
 
@@ -111,12 +104,21 @@ def main_handler(event, context):
             return "Not Renewed"
 
     # Update renewed certificate.
-    update_certificate(certificateID, 
-        f"{CONFIG_HOME}/{DOMAIN}_ecc/{DOMAIN}.key", 
-        f"{CONFIG_HOME}/{DOMAIN}_ecc/fullchain.cer",
+    update_certificate(certificateID, types, types_regions,
+        f"{CONFIG_HOME}/{domain}_ecc/{domain}.key",
+        f"{CONFIG_HOME}/{domain}_ecc/fullchain.cer",
     )
 
     return "Done"
 
-if __name__ == "__main__":
-    main_handler(None, None)
+def main_handler(event, context):
+    params = json.loads(event["Message"])
+
+    # Renew all the certificates.
+    os.system(f"{HOME}/acme.sh --cron --home {HOME} --config-home {CONFIG_HOME}")
+
+    # Check each domain.
+    for domain, domain_config in params.items():
+        print(f"[*] Handling domain: {domain}")
+        result = handle_domain(domain, domain_config)
+        print(f"[*] Result: {result}")
